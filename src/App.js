5108653e22c29376104fe8d594a6358cdb2d9284
James Geib <james@zuse.com>
2024-03-21 15:26:00 -0400

    added pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 
 const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Books />} />
+      <Route exact path="/add" element={<Add />} />
+      <Route exact path="/update" element={<Update />} />
     </Routes>
   );
-}
+};
 
 export default App;
