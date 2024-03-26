86b39bb532356fc98e0b109c5ba00212b05a56ad
James Geib <james@zuse.com>
2024-03-26 11:05:15 -0400

    constructed Add page

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
+
+    const [book, setBook] = useState({
+        title: "",
+        desc: "",
+        price: null,
+        cover: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setBook(prev => ({...prev, [e.target.name]: e.target.value}));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("http://localhost:8800/books", book);
+            navigate("/");
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    console.log(book);
+
+    return (
+        <div className="form">
+            <h1>Add New Book</h1>
+            <div style={{margin: "2% 0"}}>
+                <input type="text" placeholder="title" onChange={handleChange} name="title" />
+            </div>
+            <div style={{margin: "2% 0"}}>
+                <input type="text" placeholder="desc" onChange={handleChange} name="desc" />
+            </div>
+            <div style={{margin: "2% 0"}}>
+                <input type="number" placeholder="price" onChange={handleChange} name="price" />
+            </div>
+            <div style={{margin: "2% 0"}}>
+                <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
+            </div>
+            <button onClick={handleSubmit} type="submit">Add Book</button>
+        </div>
+    )
 };
 
 export default Add;
