49fecbe950835bb12db2b82f9f423fcf621a0b7a
James Geib <james@zuse.com>
2024-03-26 10:06:47 -0400

    rendered Book data on Books page

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try {
+                const res = await axios.get("http://localhost:8800/books");
+                setBooks(res.data);
+            } catch (err) {
+                console.log(err);
+            };
+        }
+        fetchAllBooks();
+    }, []);
+
+    return (
+        <div>
+            <h1>Cook the Books</h1>
+            <div className="books">
+                {books.map((book) => (
+                    <div className="book" key={book.id}>
+                        {book.cover && <img src={book.cover} alt={book.title} />}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>{book.price}</span>
+                    </div>
+                ))}
+            </div>
+            <button><Link to="/add">Add new book</Link></button>
+        </div>
+    );
 };
 
 export default Books;
